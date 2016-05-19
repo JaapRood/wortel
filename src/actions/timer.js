fadef9dc7873c8af1316f1223ee442ce408dead1
Jaap Rood <jaap@jaaprood.nl>
2016-05-19 17:05:58 +1000

    Implementing first crude version of the injecting of a `TIMER_STOP` action when it's supposed to be finished running

--- src/actions/timer.js
+++ src/actions/timer.js
@@ -3,10 +3,33 @@ const T = require('time-sugar')
 
 const TimerReducer = require('app/reducers/timer')
 
-exports.start = () => {
+const internals = {}
+
+// TODO: We probably shouldn't keep state here. Consider moving it elsewhere, like a service / middleware
+internals.timer = null
+internals.timerId = null
+
+exports.start = () => (dispatch, getState) => {
 	const length = T(5).seconds().valueOf()
 
-	return CreateAction(TimerReducer.START, { length: length })
+	if (internals.timer) {
+		clearTimeout(internals.timer)
+		internals.timer = null
+		internals.timerId = null
+	}
+
+	dispatch(CreateAction(TimerReducer.START, { length: length }))
+
+	const createdTimer = TimerReducer.getFromState(getState())
+
+	internals.timerId = createdTimer.get('id')
+	internals.timer = setTimeout(() => {
+		const currentTimerId = TimerReducer.getFromState(getState()).get('id')
+
+		if (internals.timerId !== currentTimerId) return; // ignore, this timer no longer exists
+
+		dispatch(exports.stop())
+	}, length - (Date.now() - createdTimer.get('startedAt')))
 }
 
 exports.stop = () => {
