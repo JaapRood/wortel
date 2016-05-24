4348a6f78f6b14b257192d0cde9b8ff09a045a13
Jaap Rood <jaap@jaaprood.nl>
2016-05-24 10:39:21 +1000

    Fixed the app reducer

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -1,8 +1,7 @@
 const ReduxImmutable = require('redux-immutable')
 
 const combinedReducers = ReduxImmutable.combineReducers({
-	sprint: require('./sprint').reduce,
-	timer: require('./timer').reduce
+	sprint: require('./sprint').reduce
 })
 
 module.exports = combinedReducers;
